028432f7bcc1b1a41c38b8eba606e0467020303c
Beslim17 <b.montiari@gmail.com>
2025-02-07 19:09:33 +0100

    Anpassungen

--- rezeptLeon.js
+++ rezeptLeon.js
@@ -1,3 +1,5 @@
+// Zutatenliste als Tabelle angeben
+
 const input = document.getElementById("input"); // <input> benötigt id="input" & value="..."
 const amounts = document.querySelectorAll("span"); // Mengenangaben in <span><span/>
 
@@ -8,6 +10,14 @@ const normalizedAmounts = [];
 for (let i = 0; i < amounts.length; i++) {
     let normalizedAmount = amounts[i].textContent / originalValue;
     normalizedAmounts.push(normalizedAmount);
+}
+
+function checkInput() {
+    if (input.value >= 1 && input.value <= 20) {
+        calculate();
+    } else {
+        alert("max 20 Portionen!");
+    }
 }
 
 function calculate() {
