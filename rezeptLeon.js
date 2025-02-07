3975812f03177cf5ea681424c339c6f6ad2ecc31
Beslim17 <b.montiari@gmail.com>
2025-02-07 14:03:59 +0100

    rezeptLeon.html aktualisiert

--- rezeptLeon.js
+++ rezeptLeon.js
@@ -1,24 +1,17 @@
 const input = document.getElementById("input");
-const amount = document.querySelectorAll("span");
+const amounts = document.querySelectorAll("span");
 
 let originalValue = input.value;
-let oldInputValue = "";
 
-function checkInput() {
-    if (input.value != oldInputValue) {
-        calculate();
-    }
+const normalizedAmounts = [];
+
+for (let i = 0; i < amounts.length; i++) {
+    let normalizedAmount = amounts[i].textContent / originalValue;
+    normalizedAmounts.push(normalizedAmount);
 }
 
 function calculate() {
-    amount.forEach(e => {
-        let normalizedAmount = "";
-        
-        if (e.innerHTML != null || e.innerHTML != "") {
-            normalizedAmount = e.innerHTML / originalValue;
-            e.innerHTML = normalizedAmount * input.value;
-
-            oldInputValue = input.value;
-        }
-    });
+    for (let i = 0; i < amounts.length; i++) {
+        amounts[i].textContent = normalizedAmounts[i] * input.value;
+    }
 }
